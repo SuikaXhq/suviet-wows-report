2108fde4028f86ef4a9a9b78d1a4ffc13b2c16f3
Xhq <xuhaoqing2013@hotmail.com>
2023-11-25 03:50:29 +0800

    feat(controller.api): 添加GroupService API

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -3,6 +3,8 @@ import { Context } from '@midwayjs/koa';
 import { AccountService } from '../service/account.service';
 import { APIResponse } from '../types/api.types';
 import { Account } from '../model/account.model';
+import { GroupService } from '../service/group.service';
+import { Group } from '../model/group.model';
 
 @Controller('/api')
 export class APIController {
@@ -11,6 +13,9 @@ export class APIController {
 
     @Inject()
     accountService: AccountService;
+
+    @Inject()
+    groupService: GroupService;
 
     @Inject()
     logger: ILogger;
@@ -48,6 +53,48 @@ export class APIController {
             };
         } catch (error) {
             this.logger.error('APIController: Error occurred when get account.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Post('/group/create')
+    async createGroup(@Body('groupName') groupName: string): Promise<APIResponse<Group>> {
+        this.logger.info(`Post /api/group/create with body {groupName: ${groupName}}`);
+        try {
+            const group = await this.groupService.createGroup(groupName);
+            this.logger.info(`APIController: Create group ${groupName} success.`);
+            this.logger.debug(group);
+            return {
+                status: 'success',
+                data: group
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when create group.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Post('/group/member/add')
+    async addGroupMember(@Body('groupId') groupId: number, @Body('accountId') accountId: number): Promise<APIResponse<void>> {
+        this.logger.info(`Post /api/group/member/add with body {groupId: ${groupId}, accountId: ${accountId}}`);
+        try {
+            const group = await this.groupService.getGroup(groupId);
+            const account = await this.accountService.getAccount(accountId);
+            await this.groupService.addMember(group, account);
+            this.logger.info(`APIController: Add account ${accountId} to group ${groupId} success.`);
+            return {
+                status: 'success'
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when add group member.');
             this.logger.error(error);
             return {
                 status: 'failed',
