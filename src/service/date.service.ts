1cbe435b8c3d84423fe716771057302d738a989e
Xhq <xuhaoqing2013@hotmail.com>
2023-11-25 00:20:27 +0800

    feat(service.date): 日期服务

---
+++ src/service/date.service.ts
@@ -0,0 +1,30 @@
+import { Provide } from "@midwayjs/core";
+
+@Provide()
+export class DateService {
+    getStartDate(date: Date): Date {
+        let startDate: Date;
+        if (date.getHours() < 3) {
+            startDate = new Date(date.getTime() - 24 * 60 * 60 * 1000);
+        } else {
+            startDate = new Date(date.getTime());
+        }
+        startDate.setHours(3, 0, 0, 0);
+        return startDate;
+    }
+
+    getEndDate(date: Date): Date {
+        let endDate: Date;
+        if (date.getHours() < 3) {
+            endDate = new Date(date.getTime());
+        } else {
+            endDate = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+        }
+        endDate.setHours(2, 59, 59, 999);
+        return endDate;
+    }
+
+    getBothEnds(date: Date): [Date, Date] {
+        return [this.getStartDate(date), this.getEndDate(date)];
+    }
+}
