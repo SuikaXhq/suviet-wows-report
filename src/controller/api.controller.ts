b38134729eb6969abf9edc7eb6e9285ba83af105
Xhq <xuhaoqing2013@hotmail.com>
2023-11-26 21:41:36 +0800

    feat(controller.api): 修改部分路由，添加ship、battle、statistics、report路由

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -1,10 +1,17 @@
-import { Inject, Controller, Post, Get, Query, Body, ILogger } from '@midwayjs/core';
+import { Inject, Controller, Post, Get, Query, Body, ILogger, Param } from '@midwayjs/core';
 import { Context } from '@midwayjs/koa';
 import { AccountService } from '../service/account.service';
 import { APIResponse } from '../types/api.types';
 import { Account } from '../model/account.model';
 import { GroupService } from '../service/group.service';
 import { Group } from '../model/group.model';
+import { ReportService } from '../service/report.service';
+import { BattleService, GetBattleOptions } from '../service/battle.service';
+import { GetStatisticsOptions, StatisticsCalculatorService } from '../service/statisticsCalculator.service';
+import { Battle, BattleTypeEnum, CalculatedBattle } from '../model/battle.model';
+import { DateService } from '../service/date.service';
+import { ShipService } from '../service/ship.service';
+import { Ship } from '../model/ship.model';
 
 @Controller('/api')
 export class APIController {
@@ -16,6 +23,21 @@ export class APIController {
 
     @Inject()
     groupService: GroupService;
+
+    @Inject()
+    reportService: ReportService;
+
+    @Inject()
+    battleService: BattleService;
+
+    @Inject()
+    statisticsCalculatorService: StatisticsCalculatorService;
+
+    @Inject()
+    dateService: DateService;
+
+    @Inject()
+    shipService: ShipService;
 
     @Inject()
     logger: ILogger;
@@ -41,8 +63,8 @@ export class APIController {
         }
     }
 
-    @Get('/account')
-    async getAccount(@Query('accountId') accountId: number): Promise<APIResponse<Account>> {
+    @Get('/account/:accountId')
+    async getAccount(@Param('accountId') accountId: number): Promise<APIResponse<Account>> {
         this.logger.info(`Get /api/account with query {accountId: ${accountId}}`);
         try {
             const account = await this.accountService.getAccount(accountId);
@@ -61,8 +83,8 @@ export class APIController {
         }
     }
 
-    @Get('/group')
-    async getGroup(@Query('groupId') groupId: number): Promise<APIResponse<Group>> {
+    @Get('/group/:groupId')
+    async getGroup(@Param('groupId') groupId: number): Promise<APIResponse<Group>> {
         this.logger.info(`Get /api/group with query {groupId: ${groupId}}`);
         try {
             const group = await this.groupService.getGroup(groupId, true);
@@ -115,6 +137,119 @@ export class APIController {
             };
         } catch (error) {
             this.logger.error('APIController: Error occurred when add group member.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Get('/report/:groupId')
+    async getReport(@Param('groupId') groupId: number, @Query('date') date: number): Promise<APIResponse<void>> {
+        this.logger.info(`Get /api/report with query {groupId: ${groupId}}`);
+        try {
+            const group = await this.groupService.getGroup(groupId, true);
+            await this.reportService.getDailyReport(group, new Date(date));
+            this.logger.info(`APIController: Get ${date} report of group ${groupId} success.`);
+            return {
+                status: 'success'
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when report group.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Get('/battle/:accountId')
+    async getBattle(
+        @Param('accountId') accountId: number,
+        @Query('date') date: number,
+        @Query('shipId') shipId: number,
+        @Query('battleType') battleType: BattleTypeEnum
+    ): Promise<APIResponse<Battle[]>> {
+        this.logger.info(`Get /api/battle with query {accountId: ${accountId}}`);
+        try {
+            const account = await this.accountService.getAccount(accountId);
+            let battles: Battle[];
+            let startTime: Date, endTime: Date;
+            if (!date) {
+                startTime = new Date(0);
+                endTime = new Date();
+            } else {
+                [startTime, endTime] = this.dateService.getBothEnds(new Date(date));
+            }
+            const getOptions: GetBattleOptions = {
+                startTime,
+                endTime,
+                ship: await this.shipService.getShip(shipId),
+                battleType
+            };
+            battles = await this.battleService.getBattle(account, getOptions);
+            this.logger.info(`APIController: Get ${battles.length} battle(s) of account ${accountId} success.`);
+            return {
+                status: 'success',
+                data: battles
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when report group.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Get('/statistics/:accountId')
+    async getStatistics(
+        @Param('accountId') accountId: number,
+        @Query('startTime') startTime: number,
+        @Query('endTime') endTime: number,
+        @Query('shipId') shipId: number,
+        @Query('battleType') battleType: BattleTypeEnum
+    ): Promise<APIResponse<CalculatedBattle>> {
+        this.logger.info(`Get /api/statistics with query {accountId: ${accountId}, startTime: ${startTime}, endTime: ${endTime}, shipId: ${shipId}, battleType: ${battleType}}`);
+        try {
+            const options: GetStatisticsOptions = {
+                startTime: startTime ? new Date(startTime) : undefined,
+                endTime: endTime ? new Date(endTime) : undefined,
+                ship: await this.shipService.getShip(shipId),
+                battleType
+            };
+            const account = await this.accountService.getAccount(accountId);
+            const statistics = await this.statisticsCalculatorService.getStatistics(account, options);
+            this.logger.info(`APIController: Get statistics of account ${accountId} success.`);
+            return {
+                status: 'success',
+                data: statistics
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when get statistics.');
+            this.logger.error(error);
+            return {
+                status: 'failed',
+                error: error.message
+            };
+        }
+    }
+
+    @Get('/ship/:shipId')
+    async getShip(@Param('shipId') shipId: number): Promise<APIResponse<Ship>> {
+        this.logger.info(`Get /api/ship with query {shipId: ${shipId}}`);
+        try {
+            const ship = await this.shipService.getShip(shipId);
+            this.logger.info(`APIController: Get ship ${shipId} success.`);
+            return {
+                status: 'success',
+                data: ship
+            };
+        } catch (error) {
+            this.logger.error('APIController: Error occurred when get ship.');
             this.logger.error(error);
             return {
                 status: 'failed',
