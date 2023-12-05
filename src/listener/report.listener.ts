b74178438dd95e7761c3c14d9703862b502674f8
Xhq <xuhaoqing2013@hotmail.com>
2023-12-05 22:52:55 +0800

    remove(listener.report): 取消在启动时更新近期report

--- src/listener/report.listener.ts
+++ src/listener/report.listener.ts
@@ -26,18 +26,18 @@ export class ReportListener {
     @Init()
     async init() {
         this.logger.info('ReportListener: initializing.');
-        const groups = await this.groupModel.find({
-            relations: ['accounts']
-        });
-        await Promise.all(groups.map(async group => {
-            // 从有单场记录开始更新
-            const firstDay = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 6);
-            for (let i = 0; i < 6; i++) {
-                const today = new Date(firstDay.getTime() + 1000 * 60 * 60 * 24 * i);
-                await this.reportService.updateDailyReport(group, today);
-                this.logger.info(`ReportListener: updated daily report for group ${group.groupName} for timestamp ${today.getTime() / 1000}.`);
-            }
-        }));
+        // const groups = await this.groupModel.find({
+        //     relations: ['accounts']
+        // });
+        // await Promise.all(groups.map(async group => {
+        //     // 从有单场记录开始更新
+        //     const firstDay = new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 6);
+        //     for (let i = 0; i < 6; i++) {
+        //         const today = new Date(firstDay.getTime() + 1000 * 60 * 60 * 24 * i);
+        //         await this.reportService.updateDailyReport(group, today);
+        //         this.logger.info(`ReportListener: updated daily report for group ${group.groupName} for timestamp ${today.getTime() / 1000}.`);
+        //     }
+        // }));
         this.updateJob = schedule.scheduleJob('0 0 3 * * *', async () => {
             const groups = await this.groupModel.find({
                 relations: ['accounts']
